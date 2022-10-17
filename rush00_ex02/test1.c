f49fdf77068d942e7f4e757d556a5ef18814dde0
Alperen <76669243+Rikarum@users.noreply.github.com>
2022-10-17 13:39:41 +0300

    Add files via upload

---
+++ rush00_ex02/test1.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test1.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerkoca <aerkoca@student.42istanbul.com.t  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 13:12:01 by aerkoca           #+#    #+#             */
+/*   Updated: 2022/10/15 13:45:38 by aerkoca          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write (1, &a, 1);
+}
+
+void	rush(int x, int y)
+{
+	int	p;
+	int	r;
+
+	r = 1;
+	while (r <= y && x > 0)
+	{
+		p = 1;
+		while (p <= x)
+		{
+			if ((p == 1 && r == 1) || (p == x && r == 1))
+				ft_putchar('A');
+			else if ((p == 1 && r == y) || (p == x && r == y))
+				ft_putchar('C');
+			else if ((p == 1) || (p == x) || (r == 1) || (r == y))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			p++;
+		}
+		ft_putchar('\n');
+		r++;
+	}
+}
+
+int	main(void)
+{
+	rush(123, 42);
+	return (0);
+}
